96c1d8bbb1bf262227bfb2396bc0a5ce910484e3
Michael Huang <sleep3030@gmail.com>
2019-11-24 02:39:56 -0800

    Adding a tutorial with some basic notes

---
+++ Cifarprocessing.py
@@ -0,0 +1,55 @@
+#import tensorflow as tf
+import numpy as np
+#from tensorflow import keras
+from keras.models import Sequential
+from keras.utils import np_utils
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.datasets import cifar10
+
+
+(X_train, Y_train), (X_test, Y_test) = cifar10.load_data()
+
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255.0
+X_test /= 255.0
+
+
+# Remember, let's consider our "x" and "y" as our true dataset, something that we know is right
+# The "Categorical" does one-hot encoding, a catch-all method of turning data into ML usable format
+Y_train = np_utils.to_categorical(Y_train)
+Y_test = np_utils.to_categorical(Y_test)
+class_num = Y_test.shape[1]
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], activation='relu',padding='same'))
+#model.add(Conv2D(32, (3, 3), input_shape=(3, 32, 32), activation='relu', padding='same'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Conv2D(64, (3, 3), activation='relu',padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Flatten())
+model.add(Dropout(0.2))
+
+model.add(Dense(256, kernel_constraint=maxnorm(3), activation='relu'))
+#model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+    
+model.add(Dense(128, kernel_constraint=maxnorm(3), activation='relu'))
+#model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(BatchNormalization())
+
+model.add(Dense(class_num))#, activation='relu')
+model.add(Activation('softmax'))
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+np.random.seed(31)
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), epochs=25, batch_size=1000)
